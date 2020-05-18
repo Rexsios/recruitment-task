ef119f3c61537376e7b95c225f068c0eb63b9830
Slawomir Palka <slawomirpalka97@gmail.com>
2020-05-18 12:28:15 +0200

    Initialize project

---
+++ src/Containers/App.tsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+import { StyledDiv } from './App.styled'
+import axios from 'axios';
+
+export default class App extends Component {
+  render() {
+    return <StyledDiv>Initialization</StyledDiv>
+  }
+}
